f74d565145fbb61174690a5a1173931a441c5e03
Myo Min Hein <myominhein.irrahub@gmail.com>
2025-07-23 18:54:38 +0700

    fix: wrapping code action bug on comment annotated widgets

--- lua/flutter-bloc/code_action.lua
+++ lua/flutter-bloc/code_action.lua
@@ -103,30 +103,22 @@ local write_widget = function(wrapped_widget, widget)
     vim.lsp.buf.format({ async = false, bufnr = widget.bufnr, })
 end
 
-local function format_widget_content(widget_text)
-    -- Is its a single line widget
-    if #widget_text == 1 then
-        return widget_text[1]
-    end
-
-    -- For multi line widget
-    local result = {}
-    for i, line in ipairs(widget_text) do
-        if i == 1 then
-            table.insert(result, line)
-        else
-            table.insert(result, "    " .. line)
-        end
-    end
-
-    return table.concat(result, " ")
-end
 
 local function apply_template(template, widget_content)
     local result = {}
     for _, line in ipairs(template) do
-        if line:find("%%s") then
-            table.insert(result, string.format(line, widget_content))
+        local start_pos, end_pos = line:find("%%s")
+        if start_pos then
+            local before_separator = string.sub(line, 1, start_pos - 1)
+            local after_separator = string.sub(line, end_pos + 1)
+
+            table.insert(result, before_separator)
+
+            for _, value in ipairs(widget_content) do
+                table.insert(result, value)
+            end
+
+            table.insert(result, after_separator)
         else
             table.insert(result, line)
         end
@@ -138,8 +130,7 @@ local wrap_with_bloc_builder = function()
     local widget = get_widget_details()
     if not widget then return end
 
-    local formatted_content = format_widget_content(widget.widget_text)
-    local wrapped_widget = apply_template(bloc_builder_template, formatted_content)
+    local wrapped_widget = apply_template(bloc_builder_template, widget.widget_text)
     write_widget(wrapped_widget, widget)
 end
 
@@ -147,8 +138,7 @@ local wrap_with_bloc_listener = function()
     local widget = get_widget_details()
     if not widget then return end
 
-    local formatted_content = format_widget_content(widget.widget_text)
-    local wrapped_widget = apply_template(bloc_listener_template, formatted_content)
+    local wrapped_widget = apply_template(bloc_listener_template, widget.widget_text)
     write_widget(wrapped_widget, widget)
 end
 
@@ -156,8 +146,7 @@ local wrap_with_bloc_provider = function()
     local widget = get_widget_details()
     if not widget then return end
 
-    local formatted_content = format_widget_content(widget.widget_text)
-    local wrapped_widget = apply_template(bloc_provider_template, formatted_content)
+    local wrapped_widget = apply_template(bloc_provider_template, widget.widget_text)
     write_widget(wrapped_widget, widget)
 end
 
@@ -165,8 +154,7 @@ local wrap_with_bloc_selector = function()
     local widget = get_widget_details()
     if not widget then return end
 
-    local formatted_content = format_widget_content(widget.widget_text)
-    local wrapped_widget = apply_template(bloc_selector_template, formatted_content)
+    local wrapped_widget = apply_template(bloc_selector_template, widget.widget_text)
     write_widget(wrapped_widget, widget)
 end
 
@@ -174,8 +162,7 @@ local wrap_with_bloc_consumer = function()
     local widget = get_widget_details()
     if not widget then return end
 
-    local formatted_content = format_widget_content(widget.widget_text)
-    local wrapped_widget = apply_template(bloc_consumer_template, formatted_content)
+    local wrapped_widget = apply_template(bloc_consumer_template, widget.widget_text)
     write_widget(wrapped_widget, widget)
 end
 
